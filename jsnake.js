b3eda7e0fdcdc2516fcb7544388408dc0ffe36f8
NBprojekt <bartko.norbert@gmail.com>
2017-09-04 01:43:15 +0200

    Upload version 0.1.3

--- jsnake.js
+++ jsnake.js
@@ -1,11 +1,11 @@
 /* 
       This file is part of JSnake.
   
-     Arenzia is free software: you can redistribute it and/or modify
+     JSnake is free software: you can redistribute it and/or modify
      it under the terms of the GNU General Public License as published by
      the Free Software Foundation, either version 3 of the License.
   
-     Arenzia is distributed in the hope that it will be useful,
+     JSnake is distributed in the hope that it will be useful,
      but WITHOUT ANY WARRANTY; without even the implied warranty of
      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
      GNU General Public License for more details.
